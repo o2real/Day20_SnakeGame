ba018adff9c80b0e208ac6bde850ad115d2b671f
Kingcode <136673119+o2real@users.noreply.github.com>
2024-07-09 20:45:54 +0900

    snake body(3)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -6,27 +7,21 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-start_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+snake = Snake()
 
-for position in start_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 # segment_1 = Turtle(shape="square")
 # segment_1.color("white")
