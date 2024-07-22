882327361f8a7a31345c816b57e94150bfde71a7
Kingcode <136673119+o2real@users.noreply.github.com>
2024-07-22 20:15:01 +0900

    add data

--- scoreboard.py
+++ scoreboard.py
@@ -8,11 +8,12 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
-        self.high_score = 0
         self.penup()
         self.goto(0, 260)
-        self.score = 0
         self.hideturtle()
         self.update_score()
 
@@ -24,9 +25,11 @@ class Scoreboard(Turtle):
         self.score += 1
         self.update_score()
 
-    def rest(self):
+    def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
 
